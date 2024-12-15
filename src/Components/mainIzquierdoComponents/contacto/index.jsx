5455bf001e72927bd668cb5ffef74caf3c1358e2
niltonGold <niltonenrique3000@hotmail.com>
2024-12-15 18:29:39 +0100

    contacto

--- src/Components/mainIzquierdoComponents/contacto/index.jsx
+++ src/Components/mainIzquierdoComponents/contacto/index.jsx
@@ -1,51 +1,85 @@
-import React, { useState } from 'react';
-import './styles.css';
+// import React, { useState } from 'react';
+// import './styles.css';
+
+// export const Contacto = () => {
+//     const [tooltipVisible, setTooltipVisible] = useState(true); 
+//     const phoneNumber = "34650347741"; 
+//     const message = "Hola Nilton me gustaría ponerme en contacto contigo";
+//     const email = "niltonenrique3000@hotmail.com";
+
+//     const handleClick = () => {
+//         setTooltipVisible(false);
+//         setTimeout(() => setTooltipVisible(true), 100); 
+
+//         const whatsappURL = `https://api.whatsapp.com/send?phone=${phoneNumber}&text=${encodeURIComponent(message)}`;
+//         window.open(whatsappURL, "_blank");
+//     };
+
+//     const handleWhatsAppClick = () => {
+//         const whatsappURL = `https://api.whatsapp.com/send?phone=${phoneNumber}&text=${encodeURIComponent(message)}`;
+//         window.open(whatsappURL, "_blank");
+//     };
+
+//     const handleEmailClick = () => {
+//         navigator.clipboard.writeText(email);
+//         alert('Correo copiado al portapapeles');
+//     };
+
+//     const handleLinkedinClick = () => {
+//         const linkedinURL = "https://www.linkedin.com/in/nilton-medina/";
+//         window.open(linkedinURL, "_blank");
+//     };
+
+//     return (
+//         <>
+//             <div className='Contacto-container'>
+//                 <div className='contacto-titulo'>CONTACTO</div>
+//                 <div className='contacto-parrafo-container'>
+//                     <div 
+//                         className='contacto-enlace-container' 
+//                         data-tooltip={tooltipVisible ? "Haz clic para copiar" : ""}
+//                         onClick={handleClick}
+//                         onTouchStart={(e) => e.preventDefault()}
+//                     >
+//                         <div className='contacto-icono contacto-telefono' />
+//                         <div className='contacto-enlace'>650347741</div>
+//                     </div>
+//                 </div>
+//             </div>
+//         </>
+//     );
+// };
+
+
+
+import React, { useState } from "react";
+import "./styles.css";
 
 export const Contacto = () => {
-    const [tooltipVisible, setTooltipVisible] = useState(true); // Controla la visibilidad del tooltip
+  const [showTooltip, setShowTooltip] = useState(true); // Estado para el tooltip
     const phoneNumber = "34650347741"; 
     const message = "Hola Nilton me gustaría ponerme en contacto contigo";
     const email = "niltonenrique3000@hotmail.com";
-
-    const handleClick = () => {
-        setTooltipVisible(false); // Oculta el tooltip al hacer clic
-        setTimeout(() => setTooltipVisible(true), 100); // Restablece el tooltip después de un tiempo
-
-        const whatsappURL = `https://api.whatsapp.com/send?phone=${phoneNumber}&text=${encodeURIComponent(message)}`;
+  const handleClick = () => {
+    setShowTooltip(false); // Oculta el tooltip al hacer clic
+    setTimeout(() => setShowTooltip(true), 100); // Restablece el estado después de un breve lapso
+            const whatsappURL = `https://api.whatsapp.com/send?phone=${phoneNumber}&text=${encodeURIComponent(message)}`;
         window.open(whatsappURL, "_blank");
-    };
+  };
 
-    const handleWhatsAppClick = () => {
-        const whatsappURL = `https://api.whatsapp.com/send?phone=${phoneNumber}&text=${encodeURIComponent(message)}`;
-        window.open(whatsappURL, "_blank");
-    };
-
-    const handleEmailClick = () => {
-        navigator.clipboard.writeText(email);
-        alert('Correo copiado al portapapeles');
-    };
-
-    const handleLinkedinClick = () => {
-        const linkedinURL = "https://www.linkedin.com/in/nilton-medina/";
-        window.open(linkedinURL, "_blank");
-    };
-
-    return (
-        <>
-            <div className='Contacto-container'>
-                <div className='contacto-titulo'>CONTACTO</div>
-                <div className='contacto-parrafo-container'>
-                    <div 
-                        className='contacto-enlace-container' 
-                        data-tooltip={tooltipVisible ? "Haz clic para copiar" : ""}
-                        onClick={handleClick}
-                        onTouchStart={(e) => e.preventDefault()}
-                    >
-                        <div className='contacto-icono contacto-telefono' />
-                        <div className='contacto-enlace'>650347741</div>
-                    </div>
-                </div>
-            </div>
-        </>
-    );
+  return (
+    <div className="Contacto-container">
+      <div className="contacto-titulo">CONTACTO</div>
+      <div className="contacto-parrafo-container">
+        <div
+          className="contacto-enlace-container"
+          data-tooltip={showTooltip ? "Haz clic para copiar" : ""}
+          onClick={handleClick}
+        >
+          <div className="contacto-icono contacto-telefono" />
+          <div className="contacto-enlace">650347741</div>
+        </div>
+      </div>
+    </div>
+  );
 };
